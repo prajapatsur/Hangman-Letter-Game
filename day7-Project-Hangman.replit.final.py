a88e9946ccc6dd7fadaec802b791b732bdd2ec62
prajapatsur <100709956+prajapatsur@users.noreply.github.com>
2022-06-17 15:46:44 +0530

    Add files via upload

---
+++ day7-Project-Hangman.replit.final.py
@@ -0,0 +1,318 @@
+logo = ''' 
+ _                                             
+| |                                            
+| |__   __ _ _ __   __ _ _ __ ___   __ _ _ __  
+| '_ \ / _` | '_ \ / _` | '_ ` _ \ / _` | '_ \ 
+| | | | (_| | | | | (_| | | | | | | (_| | | | |
+|_| |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
+                    __/ |                      
+                   |___/    '''
+print(logo)
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+word_list = [
+'abruptly',
+'absurd',
+'abyss',
+'affix',
+'askew',
+'avenue',
+'awkward',
+'axiom',
+'azure',
+'bagpipes',
+'bandwagon',
+'banjo',
+'bayou',
+'beekeeper',
+'bikini',
+'blitz',
+'blizzard',
+'boggle',
+'bookworm',
+'boxcar',
+'boxful',
+'buckaroo',
+'buffalo',
+'buffoon',
+'buxom',
+'buzzard',
+'buzzing',
+'buzzwords',
+'caliph',
+'cobweb',
+'cockiness',
+'croquet',
+'crypt',
+'curacao',
+'cycle',
+'daiquiri',
+'dirndl',
+'disavow',
+'dizzying',
+'duplex',
+'dwarves',
+'embezzle',
+'equip',
+'espionage',
+'euouae',
+'exodus',
+'faking',
+'fishhook',
+'fixable',
+'fjord',
+'flapjack',
+'flopping',
+'fluffiness',
+'flyby',
+'foxglove',
+'frazzled',
+'frizzled',
+'fuchsia',
+'funny',
+'gabby',
+'galaxy',
+'galvanize',
+'gazebo',
+'giaour',
+'gizmo',
+'glowworm',
+'glyph',
+'gnarly',
+'gnostic',
+'gossip',
+'grogginess',
+'haiku',
+'haphazard',
+'hyphen',
+'iatrogenic',
+'icebox',
+'injury',
+'ivory',
+'ivy',
+'jackpot',
+'jaundice',
+'jawbreaker',
+'jaywalk',
+'jazziest',
+'jazzy',
+'jelly',
+'jigsaw',
+'jinx',
+'jiujitsu',
+'jockey',
+'jogging',
+'joking',
+'jovial',
+'joyful',
+'juicy',
+'jukebox',
+'jumbo',
+'kayak',
+'kazoo',
+'keyhole',
+'khaki',
+'kilobyte',
+'kiosk',
+'kitsch',
+'kiwifruit',
+'klutz',
+'knapsack',
+'larynx',
+'lengths',
+'lucky',
+'luxury',
+'lymph',
+'marquis',
+'matrix',
+'megahertz',
+'microwave',
+'mnemonic',
+'mystify',
+'naphtha',
+'nightclub',
+'nowadays',
+'numbskull',
+'nymph',
+'onyx',
+'ovary',
+'oxidize',
+'oxygen',
+'pajama',
+'peekaboo',
+'phlegm',
+'pixel',
+'pizazz',
+'pneumonia',
+'polka',
+'pshaw',
+'psyche',
+'puppy',
+'puzzling',
+'quartz',
+'queue',
+'quips',
+'quixotic',
+'quiz',
+'quizzes',
+'quorum',
+'razzmatazz',
+'rhubarb',
+'rhythm',
+'rickshaw',
+'schnapps',
+'scratch',
+'shiv',
+'snazzy',
+'sphinx',
+'spritz',
+'squawk',
+'staff',
+'strength',
+'strengths',
+'stretch',
+'stronghold',
+'stymied',
+'subway',
+'swivel',
+'syndrome',
+'thriftless',
+'thumbscrew',
+'topaz',
+'transcript',
+'transgress',
+'transplant',
+'triphthong',
+'twelfth',
+'twelfths',
+'unknown',
+'unworthy',
+'unzip',
+'uptown',
+'vaporize',
+'vixen',
+'vodka',
+'voodoo',
+'vortex',
+'voyeurism',
+'walkway',
+'waltz',
+'wave',
+'wavy',
+'waxy',
+'wellspring',
+'wheezy',
+'whiskey',
+'whizzing',
+'whomever',
+'wimpy',
+'witchcraft',
+'wizard',
+'woozy',
+'wristwatch',
+'wyvern',
+'xylophone',
+'yachtsman',
+'yippee',
+'yoked',
+'youthful',
+'yummy',
+'zephyr',
+'zigzag',
+'zigzagging',
+'zilch',
+'zipper',
+'zodiac',
+'zombie',
+]
+from replit import clear
+import random
+chosen_word = random.choice(word_list)
+word_length = len(chosen_word)
+end_of_game = False
+print(chosen_word)
+lives = 6
+display = []
+group = []
+for i in range(word_length):
+    display.append('_')
+while end_of_game==False:
+    guess = input("Enter a word: ").lower()
+
+    for i in range(word_length):
+        if guess == chosen_word[i]:
+            display[i]=guess
+    if guess not in group:
+        if guess not in chosen_word:
+            lives -= 1
+        group.append(guess)
+    else:
+        print("You have already entered that word.")
+    clear()
+    print(" ".join(display))
+    #print("Live= ",lives)
+    #print(group)
+    print(stages[lives])
+    # Defining end condition
+    if lives==0:
+        end_of_game = True
+        print("You lose")
+    elif '_' not in display:
+        end_of_game = True
+        print("You win.")
+    else:
+        end_of_game = False
